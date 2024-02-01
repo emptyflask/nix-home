bbe4aad0972afed495ea30e8e49324aab98328d9
Jon Roberts <jon@emptyflask.net>
2024-02-01 13:31:40 -0600

    override electron version for obsidian

--- home-nixos.nix
+++ home-nixos.nix
@@ -3,6 +3,9 @@ args@{ config, pkgs, lib, ... }:
 {
   nixpkgs.config.allowUnfree = true;
 
+  nixpkgs.config.permittedInsecurePackages =
+    lib.optional (pkgs.obsidian.version == "1.5.3") "electron-25.9.0";
+
   # nixpkgs.config.packageOverrides = self : rec {
   #   blender = self.blender.override {
   #     cudaSupport = true;
